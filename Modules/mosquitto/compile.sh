16fac8731aad560f084ce2f1ec13054987383f32
Philipp <26636274+PhilippFr@users.noreply.github.com>
2025-06-01 13:02:30 +0200

    fix mosquitto build

--- Modules/mosquitto/compile.sh
+++ Modules/mosquitto/compile.sh
@@ -9,6 +9,7 @@ export CC=/bin/mips-linux-gnu-gcc
 export AR=/bin/mips-linux-gnu-ar
 export CXX=/bin/mips-linux-gnu-g++
 export LDFLAGS="${LDFLAGS} -lrt -lssl -ltls -lcrypto -lpthread"
+export CFLAGS="${CFLAGS} -std=gnu99"
 
 if [ ! -d mosquitto/.git ]
 then
@@ -23,5 +24,5 @@ cp client/mosquitto_sub ${INSTALLDIR}/bin/mosquitto_sub.bin
 cp client/mosquitto_pub ${INSTALLDIR}/bin/mosquitto_pub.bin
 cp src/mosquitto ${INSTALLDIR}/bin/mosquitto.bin
 cp lib/libmosquitto.so.1 ${INSTALLDIR}/lib
-ln -s ${INSTALLDIR}/lib/libmosquitto.so.1 ${INSTALLDIR}/lib/libmosquitto.so
+ln -sf ${INSTALLDIR}/lib/libmosquitto.so.1 ${INSTALLDIR}/lib/libmosquitto.so
 cp include/* ${INSTALLDIR}/include
